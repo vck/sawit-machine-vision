b5cb9ff133035a8a9e110e724cb049b2e0a51bf0
vickydasta <vickydasta@gmail.com>
2017-09-22 15:54:43 +0700

    add gray value

--- server/serve.py
+++ server/serve.py
@@ -7,33 +7,62 @@ from flask import (
     url_for,
     flash)
 
+from PIL import Image
+
 DEV_MACHINE = 'X86_64'
 PROD_MACHINE = 'armv7l'
 PROD_MODE = False
-
+DEBUG = True
 
 if os.uname()[-1] == PROD_MACHINE:
     from PIL import Image
     from picamera import PiCamera
     PROD_MODE = True
+    DEBUG=False
 
 
 app = Flask(__name__)
 
 
+app.config["UPLOAD_FOLDER"] = '/home/vickydasta/Pictures'
+
 @app.route("/", methods=["GET", "POST"])
 def capture_image():
+
+    # PRODUCTION ENVIRONMENT
     if PROD_MODE == True:
         if request.method == "POST":
             camera.capture("static/test.jpg")
             im = Image.open("static/test.jpg")
             rgb_im = im.convert('RGB')
-            r,g,b = rgb_im.getpixel((1, 1))
-            return render_template("index.html", data=(r, g, b, "test.jpg"))
+            R,G,B = rgb_im.getpixel((1, 1))
+            grey_value = (0.3 * R, 0.59 * G, 0.11 * B)
+            return render_template("prod.html", data=(R, G, B, "test.jpg"), grey_value=grey_value)
         else:
             return render_template("prod.html")
+
+
+    # DEVELOPMENT ENVIRONMENT
+    if request.method == "POST":
+        # upload file
+        # process uploaded file with PIL
+        # return rgb and gray value (W, D, )
+
+        if 'file' not in request.files:
+            flash("no file found")
+            return redirect(url_for('capture_image'))
+
+        files = request.files['file']
+        files.save(os.path.join(app.config['UPLOAD_FOLDER'], files.filename))
+
+        im = Image.open('/home/vickydasta/Pictures/'+files.filename)
+        rgb_im = im.convert('RGB')
+        R,G,B = rgb_im.getpixel((1, 1))
+        grey_value = (0.3 * R, 0.59 * G, 0.11 * B)
+        return render_template('dev.html', rgb=rgb_im, grey_value=grey_value)
+
     else:
         return render_template("dev.html")
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=8000)
+    app.run('0.0.0.0', port=8000, debug=DEBUG)
