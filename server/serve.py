1dfb2314c0db254d7234d0e888071461aaf30b8f
vickydasta <vickydasta@gmail.com>
2017-10-06 17:10:00 +0700

    zoom and sensor mode

--- server/serve.py
+++ server/serve.py
@@ -5,10 +5,7 @@ from datetime import strftime
 from flask import (
     Flask,
     render_template,
-    request,
-    redirect,
-    url_for,
-    flash)
+    request)
 
 from PIL import Image
 from time import strftime
@@ -21,9 +18,9 @@ DEBUG = True
 if os.uname()[-1] == PROD_MACHINE:
     from PIL import Image
     from picamera import PiCamera
-    camera = PiCamera()
+    camera = PiCamera(sensor_mode=5)
     PROD_MODE = True
-    DEBUG=False
+    DEBUG = False
 
 
 app = Flask(__name__)
@@ -31,7 +28,7 @@ app = Flask(__name__)
 
 def generate_filename():
     timestamps = strftime("%m%d%Y-%H%M%S")
-    return timestamps+'.jpg'
+    return timestamps + '.jpg'
 
 @app.route("/", methods=["GET", "POST"])
 def capture_image():
@@ -40,12 +37,14 @@ def capture_image():
     if PROD_MODE == True:
         if request.method == "POST":
             filename = generate_filename()
+            camera.resolution = (1280, 720)
+            camera.crop =  (0.25, 0.4, 1, 2)
             camera.capture("static/"+filename))
             im = Image.open("static/"+filename)
             rgb_im = im.convert('RGB')
             R,G,B = rgb_im.getpixel((1, 1))
             grey_value = (0.3 * R, 0.59 * G, 0.11 * B)
-            return render_template('prod.html', rgb=(R,G,B), grey_value=grey_value, filename=filename)
+            return render_template('prod.html', rgb=(R, G, B), grey_value=grey_value, filename=filename)
 
         return render_template("prod.html")
 
@@ -54,7 +53,7 @@ def capture_image():
     if request.method == "POST":
         R, G, B = 12, 34, 56
         grey_value = (0.3 * R, 0.59 * G, 0.11 * B)
-        return render_template('dev.html', rgb=(R,G,B), grey_value=grey_value)
+        return render_template('dev.html', rgb=(R, G, B), grey_value=grey_value)
 
     return render_template("dev.html")
 
